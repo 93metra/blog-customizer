286c0bb3060ee8dabf1b1e6144d8c34493904ef3
Artem Voevodin <voevodin320@gmail.com>
2024-09-27 13:24:22 +0200

    layout done

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,109 @@
+import { useState } from 'react';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Text } from '../text';
+import { Select } from "../select/Select";
+import { Separator } from '../separator';
+import { RadioGroup } from '../radio-group';
+import { fontFamilyOptions, fontColors, backgroundColors, contentWidthArr, fontSizeOptions } from 'src/constants/articleProps';
+import { OptionType } from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+
+	// font-family selection
+	const [selectedFont, setSelectedFont] = useState<typeof fontFamilyOptions[0]>(fontFamilyOptions[0]);
+  const handleFontChange = (option: OptionType) => {
+    setSelectedFont(option);
+    console.log('Selected font:', option.value);
+		console.log(option)
+  };
+
+	// font-color selection
+	const [selectedFontColor, setSelectedFontColor] = useState<typeof fontColors[0]>(fontColors[0]);
+  const handleFontColorChange = (option: typeof fontColors[0]) => {
+    setSelectedFontColor(option);
+		console.log(option);
+  };
+
+	// background-color selection
+	const [selectedBackgroundColor, setSelectedBackgroundColor] = useState<typeof backgroundColors[0]>(backgroundColors[0]);
+	const handleBackgroundColorChange = (option: typeof backgroundColors[0]) => {
+		setSelectedBackgroundColor(option);
+		console.log(option);
+	};
+
+	// content-width selection
+	const [selectedContentWidth, setSelectedContentWidth] = useState<typeof contentWidthArr[0]>(contentWidthArr[0]);
+	const handleContentWidthChange = (option: typeof contentWidthArr[0]) => {
+		setSelectedContentWidth(option);
+		console.log(option);
+	};
+
+	// font-size selection
+	const [selectedFontSize, setSelectedFontSize] = useState<typeof fontSizeOptions[0]>(fontSizeOptions[0]);
+	const handleFontSizeChange = (option: typeof fontSizeOptions[0]) => {
+		setSelectedFontSize(option);
+		console.log(option);
+	};
+
 	return (
 		<>
-			<ArrowButton />
+			<ArrowButton/>
 			<aside className={styles.container}>
 				<form className={styles.form}>
+					<div className={styles.wrapperWithMarginBottom}>
+						<Text as="h1" uppercase={true} weight={800} size={31}>Задайте параметры</Text>
+					</div>
+					<div className={styles.wrapperWithMarginBottom}>
+						<Select
+							options={fontFamilyOptions}
+							selected={selectedFont}
+							onChange={handleFontChange}
+							placeholder={selectedFont.title} 
+							title="Шрифт" 
+						/>
+					</div>
+					<div className={styles.wrapperWithMarginBottom}>
+						<RadioGroup
+							name="font-size"
+							options={fontSizeOptions}
+							selected={selectedFontSize}
+							onChange={handleFontSizeChange}
+							title="Размер шрифта" 
+						/>
+					</div>
+					<div className={styles.wrapperWithMarginBottom}>
+						<Select
+							options={fontColors}
+							selected={selectedFontColor}
+							onChange={handleFontColorChange}
+							placeholder={selectedFontColor.title} 
+							title="Цвет шрифта"
+						/>
+					</div>
+					<div className={styles.wrapperWithMarginBottom}>
+						<Separator/>
+					</div>
+					<div className={styles.wrapperWithMarginBottom}>
+						<Select
+							options={backgroundColors}
+							selected={selectedBackgroundColor}
+							onChange={handleBackgroundColorChange}
+							placeholder={selectedBackgroundColor.title}
+							title="Цвет фона" 
+						/>
+					</div>
+					<div className={styles.wrapperWithMarginBottom}>
+						<Select
+							options={contentWidthArr}
+							selected={selectedContentWidth}
+							onChange={handleContentWidthChange}
+							placeholder={selectedContentWidth.title} 
+							title="Ширина контента" 
+						/>
+					</div>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
